5948df51ed210b4de36c8ee2206ac3ba5bf32e9f
Kn0ckout-coder <rodrigocostacarneiro12@gmail.com>
2024-01-18 23:54:42 +0000

    search bar completed

--- Index/index.js
+++ Index/index.js
@@ -21,4 +21,39 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+/* function search() {
+    let input = document.getElementById('searchInput').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('recipe');
+ 
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+        x[i].style.display = "none";
+        }
+        else {
+        x[i].style.display = "list-item";
+        }
+    }
+} */
+
+function search() {
+    let filter = document.getElementById('searchInput').value.toUpperCase();
+    let item = document.querySelectorAll('.divrecipe');
+    let l = document.getElementsByTagName('h3');
+
+    for(var i = 0;i <= l.length;i++){
+        let a = item[i].getElementsByTagName('h3')[0];
+        let value = a.innerHTML || a.innerText || a.textContent;
+        if(value.toUpperCase().indexOf(filter) > -1) {
+            item[i].style.display = "";
+        }
+        else
+        {
+            item[i].style.display = "none";
+        }
+    }
+}
+
+  
+
 
