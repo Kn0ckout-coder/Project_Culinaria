88582a26455965f059e9a1184ebdaedc44231687
Kn0ckout-coder <rodrigocostacarneiro12@gmail.com>
2024-05-20 23:46:27 +0100

    finalização e resolução de bugs da 1 parte

--- www/scripts/main.js
+++ www/scripts/main.js
@@ -1,68 +1,96 @@
-const nameMap = {
-    'arroz': 'Arroz Doce',
-    'bolinhos': 'Bolinhos de Peixe',
-    'bolodecarne': 'Bolo de Carne',
-    'caldo': 'Caldo de Peixe',
-    'cebola': 'Cebola Frita Caramelizada',
-    'cheese': 'Cheesecake com Dióspiro',
-    'feijoada': 'Feijoada do Mar',
-    'linguica': 'Linguiça Recheada na AirFryer',
-    'mousse': 'Mousse de Cacau',
-    'panquecas': 'Panquecas Americanas',
-    'salmao': 'Pasta de Salmao',
-    'strogonoff': 'Strogonoff'
-};
-fetch('/receitas.json')
-    .then(response => response.json())
-    .then(data => {
-        const pageName = window.location.pathname.split('/').pop().replace('.html', '');
-        const recipeName = nameMap[pageName];
+document.addEventListener('DOMContentLoaded', () => {
+    class RecipeLoader {
+        constructor() {
+            this.recipes = [];
+            this.nameMap = {
+                'arroz': 'Arroz Doce',
+                'bolinhos': 'Bolinhos de Peixe',
+                'bolodecarne': 'Bolo de Carne Moída',
+                'caldo': 'Caldo de Peixe',
+                'cebola': 'Cebola Frita Caramelizada',
+                'cheese': 'Cheesecake com Dióspiro',
+                'feijoada': 'Feijoada do Mar',
+                'linguica': 'Linguiça Recheada na Airfryer',
+                'mousse': 'Mousse de Cacau',
+                'panquecas': 'Panquecas Americanas',
+                'salmao': 'Pasta de Salmão',
+                'strogonoff': 'Strogonoff'
+            };
+        }
 
-        if (recipeName){
-            const selectedRecipe = data.find(receita => receita.name === recipeName);
+        fetchRecipes() {
+            fetch('/receitas.json')
+                .then(response => response.json())
+                .then(data => {
+                    this.recipes = data;
+                    this.loadRecipeByPage();
+                })
+                .catch(error => console.error('Erro ao carregar dados:', error));
+        }
 
-            if (selectedRecipe) {
-                console.log(selectedRecipe);
-                const titleEl = document.querySelector('main');
-                const sectionEl = document.querySelector('section');
-                const titlePart = `
+        getPageName() {
+            return window.location.pathname.split('/').pop().replace('.html', '');
+        }
+
+        getRecipeName(pageName) {
+            return this.nameMap[pageName];
+        }
+
+        loadRecipeByPage() {
+            const pageName = this.getPageName();
+            const recipeName = this.getRecipeName(pageName);
+
+            if (recipeName) {
+                const selectedRecipe = this.recipes.find(receita => receita.name === recipeName);
+
+                if (selectedRecipe) {
+                    this.displayRecipe(selectedRecipe);
+                } else {
+                    console.error('Receita não encontrada.');
+                }
+            }
+        }
+
+        displayRecipe(recipe) {
+            const titleEl = document.querySelector('main');
+            const sectionEl = document.querySelector('section');
+
+            const titlePart = `
                 <br>
-                <h2>${selectedRecipe.name}</h2>
-                `;
-                titleEl.insertAdjacentHTML('beforebegin', titlePart);
-                const firstRecipePart= `
-                    <img src="${selectedRecipe.image}">
-                    <h3>${selectedRecipe.name}</h3>
-                    <p>${selectedRecipe.description}</p>
-                `;
-                //fazer a correção dessa parte das estrelas e otimizar o sistema de ficheiros
+                <h2>${recipe.name}</h2>
+            `;
+            titleEl.insertAdjacentHTML('beforebegin', titlePart);
 
-                sectionEl.insertAdjacentHTML('afterbegin', firstRecipePart);
+            const firstRecipePart = `
+                <img src="${recipe.image}">
+                <h3>${recipe.name}</h3>
+                <p>${recipe.description}</p>
+            `;
 
-                const lastRecipePart = `
-                <br>    
+            sectionEl.insertAdjacentHTML('afterbegin', firstRecipePart);
+
+            const lastRecipePart = `
+                <br>
                 <table>
                     <tr>
-                        <th>Ingredientes para ${selectedRecipe.name}</th>
+                        <th>Ingredientes para ${recipe.name}</th>
                     </tr>
                     <tr>
                         <td colspan="2">
                         <ul>
-                            ${selectedRecipe.ingredients.map(ingrediente => `<li>${ingrediente}</li>`).join('')}
+                            ${recipe.ingredients.map(ingrediente => `<li>${ingrediente}</li>`).join('')}
                         </ul>
                         </td>
                     </tr>
                 </table>
-                <h2>Passos para fazer ${selectedRecipe.name}</h2>
+                <h2>Passos para fazer ${recipe.name}</h2>
                 <ol>
-                    ${selectedRecipe.steps.map(passo => `<li>${passo}</li>`).join('')}
+                    ${recipe.steps.map(passo => `<li>${passo}</li>`).join('')}
                 </ol>
-                `;
-                sectionEl.insertAdjacentHTML('beforeend', lastRecipePart);  
-
-                } else {
-                    console.error('Receita não encontrada.');
-                }        
-        }   
-    })
-    .catch(error => console.error('Erro ao carregar dados:', error));
+            `;
+            sectionEl.insertAdjacentHTML('beforeend', lastRecipePart);
+        }
+    }
+    const recipeLoader = new RecipeLoader();
+    recipeLoader.fetchRecipes();
+});
