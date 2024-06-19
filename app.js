4858b39331c5e6f6f48eba66072b5d411903913d
Kn0ckout-coder <rodrigocostacarneiro12@gmail.com>
2024-06-19 08:53:43 +0100

    start of ejs files

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express');
 const handler = require('./request-handlers');
 const app = express();
 const port = 8081;
+app.set('view engine', 'ejs');
 
-app.use(express.static("www"));
+app.use(express.static(__dirname + '/www'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 
 app.get('/api/recipes', (req, res) => {
     handler.query('SELECT * FROM recipes', (err, results) => {
         if (err) {
             return res.status(500).send(err);
         }
-        res.json(results);
+        res.status(200).json(results);
     });
 });
 
